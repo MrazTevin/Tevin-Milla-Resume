3dc688e2cf002afdde524054da2cfbea3abd5954
MrazTevin <millatevin@gmail.com>
2022-11-09 10:59:43 +0300

    m: hide action bar

--- app/src/main/java/com/example/resume/Resume.kt
+++ app/src/main/java/com/example/resume/Resume.kt
@@ -3,10 +3,9 @@ package com.example.resume
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class Resume : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        supportActionBar?.hide()
+        setContentView(R.layout.activity_resume)
     }
 }
